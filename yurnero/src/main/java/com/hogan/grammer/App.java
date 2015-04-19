e1e8f00a739d3478be70f56134980b75429bd60d
Hogan <hong_ping08@163.com>
2015-04-19 12:12:42 +0800

    begin

---
+++ yurnero/src/main/java/com/hogan/grammer/App.java
@@ -0,0 +1,13 @@
+package com.hogan.grammer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
